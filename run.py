21eb6ada98e028580f646ff8505e1688d12708b6
Isaac-kiptoo-kilimo <isaac.kilimo@student.moringaschool.com>
2022-04-23 17:53:11 +0300

    feature:added delete function

--- run.py
+++ run.py
@@ -6,13 +6,22 @@ from details import Account
 
 def create_account(fname,lname,username,password,email):
     '''
-    Function to create a new contact
+    Function to create a new account
     '''
     new_account =Account(fname,lname,username,password,email)
     return new_account
+
 def save_accounts(account):
-   
+    '''
+    Function to save the new account
+    '''
     account.save_account()
+
+def del_account(account):
+    '''
+    Function to delete an account
+    '''
+    account.delete_account()
 
 def main():
     print("Hello enter you Details to create an account")
