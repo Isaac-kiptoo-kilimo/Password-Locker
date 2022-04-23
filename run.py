95be1baa4250689391c7a623b84c656ae5aee0f8
Isaac-kiptoo-kilimo <isaac.kilimo@student.moringaschool.com>
2022-04-23 20:38:23 +0300

    feature:added while loop

--- run.py
+++ run.py
@@ -23,11 +23,18 @@ def del_account(account):
     '''
     account.delete_account()
 
+print('PASSWORD LOCKER APPLICATION')
+print("\n")
+
 def main():
-    print("Hello enter you Details to create an account")
+    print("Hello welcome,Enter your name to create an account")
     user_name=input()
     print(f"Hello {user_name}. what would you like to do?")
     print("\n")
+
+    while True:
+        print("Use these short codes : ca - create a new account,gp -generate new password ,cpg - get generated password by computor, da - display accounts,dp -display old password, fa -find an account, ex -exit the account list ")
+        break
 
 if __name__=="__main__":
     main()
