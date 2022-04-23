f150b249785fb3cd0366f74d9985dadf71a6bb69
Isaac-kiptoo-kilimo <isaac.kilimo@student.moringaschool.com>
2022-04-23 13:59:36 +0300

    ft:rendering the user class

---
+++ users.py
@@ -0,0 +1,9 @@
+import unittest
+
+class User:
+    def __init__(self,fname,lname,password,email):
+        self.fname=fname
+        self.lname=lname
+        self.pasaword=password
+        self.email=email
+print(User)
