46cf233bd1185525420b3226a785b6e5b6325b59
Isaac-kiptoo-kilimo <isaac.kilimo@student.moringaschool.com>
2022-04-24 17:12:49 +0300

    feature:addded credintials class

---
+++ credentials.py
@@ -0,0 +1,18 @@
+class Credentials:
+    credentials = [
+        {
+            "id": 1,
+            "user": 1,
+            "platform": "Twitter",
+            "password": "SomeTwitterPassword123"
+        },
+        {
+            "id": 2,
+            "user": 2,
+            "platform": "Facebook",
+            "password": "SomeFacebookPassword123"
+        }
+    ]
+
+    def __init__(self):
+        pass
