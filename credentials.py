90b5a44ad4f49f8383a3c927a297448f704bccfe
Isaac-kiptoo-kilimo <isaac.kilimo@student.moringaschool.com>
2022-04-24 17:30:29 +0300

    feature:delete credentials function

--- credentials.py
+++ credentials.py
@@ -34,3 +34,25 @@ class Credentials:
             if cred["id"] == int(cred_id):
                 return {"message": "Credential found", "data": cred, "statusCode": 200}
         return {"message": "Credential not found", "data": None, "statusCode": 404}
+    
+    @classmethod
+    def getUserCredentials(cls, user_id):
+        creds = []
+        for cred in cls.credentials:
+            if cred["user"] == int(user_id):
+                creds.append(cred)
+
+        return {"message": "Found Credentials", "data": creds, "statusCode": 404}
+
+    @classmethod
+    def deleteCredential(cls, cred_id):
+        res = cls.getCredential(cred_id)
+        if res["statusCode"] == 200:
+            cls.credentials.remove(res['data'])
+            return {
+                "message": "Credential deleted successfully",
+                "data": None,
+                "statusCode": 200
+            }
+        else:
+            return res
